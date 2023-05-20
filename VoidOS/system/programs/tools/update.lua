3bb9b1ce74f123a88d1e1a85301fd86cf0bb61b1
NullHarp <smithgavin814@gmail.com>
2023-05-20 14:31:31 -0400

    imrpoved updater

--- VoidOS/system/programs/tools/update.lua
+++ VoidOS/system/programs/tools/update.lua
@@ -14,6 +14,19 @@ else
     shell.run("/VoidOS/system/core/menu/mainMenu.lua tool")
 end
 
+local file = fs.open("/VoidOS/system/programs/tools/update.lua","r")
+local fileData = file.readAll()
+file.close()
+
+local fileHandle = http.get(githubPath.."VoidOS/system/programs/tools/update.lua")
+local fileHandleData = fileHandle.readAll()
+
+if fileData ~= fileHandleData then
+    local file = fs.open("/VoidOS/system/programs/tools/update.lua","w")
+    file.write(fileHandleData)
+    file.close()
+    shell.run("/VoidOS/system/programs/tools/update.lua")
+end
 
 local fileToGet = {
     "VoidOS/system/core/init.lua",
@@ -24,24 +37,34 @@ local fileToGet = {
     "VoidOS/system/core/menu/mainMenu.lua",
     "VoidOS/system/programs/tools/fileExplorer.lua",
     "VoidOS/system/programs/tools/fileTransfer.lua",
-    "VoidOS/system/programs/tools/newAccount.lua",
-    "VoidOS/system/programs/tools/update.lua"
+    "VoidOS/system/programs/tools/newAccount.lua"
 }
 
 for i,filePath in pairs(fileToGet) do
     local file = fs.open(filePath,"r")
-    local fileData = file.readAll()
+    local fileData
+    if type(file) ~= "nil" then
+        fileData = file.readAll()
+        file.close()
+    end
     local fileHandle = http.get(githubPath..filePath)
     local fileHandleData = fileHandle.readAll()
     textutils.pagedPrint(filePath)
-    if fileData ~= fileHandleData then
-        textutils.pagedPrint("File is not up to date")
-        textutils.pagedPrint("Reinstalling")
-        local file = fs.open(filePath,"w")
-        file.write(fileHandleData)
-        file.close()
+    if type(file) ~= "nil" then
+        if fileData ~= fileHandleData then
+            textutils.pagedPrint("Updating")
+            local file = fs.open(filePath,"w")
+            file.write(fileHandleData)
+            file.close()
+        else
+            print("File is up to date")
+        end
     else
-        print("File is up to date")
+        textutils.pagedPrint("File not found.")
+        textutils.pagedPrint("Reinstalling")
+        local newFile = fs.open(filePath,"w")
+        newFile.write(fileHandleData)
+        newFile.close()
     end
 end
 sleep(1)
